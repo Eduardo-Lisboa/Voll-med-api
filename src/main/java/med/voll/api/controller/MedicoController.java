e59ebaa1f87fd7984763da1ebff57d1f1975a953
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2023-06-13 20:59:00 -0300

    crud medicos finalizado

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,52 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/medico")
+public class MedicoController {
+
+    @Autowired
+    private MedioRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
+
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizar(dados);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
+
+
+}
+
+
